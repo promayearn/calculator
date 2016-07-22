31414dca66ff8f81dba2a8f43d90a38f23b698fe
Chayanit Kaensan <Chayanit.K@augmentis.biz>
2016-07-22 11:14:50 +0700

    Test Case

---
+++ app/src/main/java/com/augmentis/ayp/calculator/Calculator.java
@@ -0,0 +1,40 @@
+package com.augmentis.ayp.calculator;
+
+/**
+ * Created by Chayanit on 7/22/2016.
+ */
+public class Calculator {
+
+    String temp = "";
+    float mainNumber = 0;
+    String currenOperator;
+    float result;
+    public void pressNumber(String s) {
+        temp += s;
+    }
+
+    public void pressOperator(String s) {
+        mainNumber = Float.parseFloat(temp);
+        temp = "";
+        currenOperator = s;
+    }
+
+    public void pressEqual() {
+
+        float temp = Float.parseFloat(this.temp);
+
+        if(currenOperator.equals("+")){
+            result = mainNumber + temp;
+        } else if (currenOperator.equals("-")){
+            result = mainNumber - temp;
+        } else if (currenOperator.equals("*")){
+            result = mainNumber * temp;
+        } else if (currenOperator.equals("/")){
+            result = mainNumber / temp;
+        }
+    }
+
+    public float getResult() {
+        return result;
+    }
+}
--- app/src/test/java/com/augmentis/ayp/calculator/ExampleUnitTest.java
+++ app/src/test/java/com/augmentis/ayp/calculator/ExampleUnitTest.java
@@ -8,8 +8,54 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    float a = 2;
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test_1_plus_1_equals_2(){
+        Calculator calculator = new Calculator();
+        calculator.pressNumber("1");
+        calculator.pressOperator("+");
+        calculator.pressNumber("1");
+        calculator.pressEqual();
+
+        assertEquals(2,calculator.getResult(),0);
+    }
+
+    @Test
+    public void test_1_minus_1_equals_0(){
+        Calculator calculator = new Calculator();
+        calculator.pressNumber("1");
+        calculator.pressOperator("-");
+        calculator.pressNumber("1");
+        calculator.pressEqual();
+
+        assertEquals(0,calculator.getResult(),0);
+    }
+
+    @Test
+    public void test_4_multi_5_equals_20(){
+        Calculator calculator = new Calculator();
+        calculator.pressNumber("4");
+        calculator.pressOperator("*");
+        calculator.pressNumber("5");
+        calculator.pressEqual();
+
+        assertEquals(20,calculator.getResult(),0);
+    }
+
+    @Test
+    public void test_4_divide_2_equals_2(){
+        Calculator calculator = new Calculator();
+        calculator.pressNumber("4");
+        calculator.pressOperator("/");
+        calculator.pressNumber("2");
+        calculator.pressEqual();
+
+        assertEquals(2,calculator.getResult(),0);
     }
 }
